0bc1afc713f4bcafa756231a382dc4585356d7cf
336 <3k1057336@gmail.com>
2026-04-06 17:45:50 +0900

    Update 2399.py

--- lec9/2399.py
+++ lec9/2399.py
@@ -1,7 +1,9 @@
 n=int(input())
 x=list(map(int,input().split()))
+x.sort()
+
 Nsum=0
 for i in range(n):
-    for j in range(n):
-        Nsum+=abs(x[i]-x[j])
-print(Nsum)
+    Nsum += (i - (n - 1 - i)) * x[i]
+        
+print(Nsum*2)
