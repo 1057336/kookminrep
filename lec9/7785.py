40d8ddde9a028bacbda331ad14e81a48459d5d88
336 <3k1057336@gmail.com>
2026-04-07 11:42:09 +0900

    Update 7785.py

--- lec9/7785.py
+++ lec9/7785.py
@@ -6,5 +6,6 @@ for i in range(turn):
         people.append(peoplelog[0])
     elif peoplelog[1]=='leave'and peoplelog[0] in people:
         people.remove(peoplelog[0])
+people.sort(reverse=True)
 for i in range(len(people)):
     print(people[i])
