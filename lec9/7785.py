740cbc70f48d2204d9594c8e7ba70c403bd9b9bb
336 <3k1057336@gmail.com>
2026-04-07 11:33:14 +0900

    Create 7785.py

---
+++ lec9/7785.py
@@ -0,0 +1,10 @@
+turn=int(input())
+people=[]
+for i in range(turn):
+    peoplelog=input().split()
+    if peoplelog[1]=='enter':
+        people.append(peoplelog[0])
+    elif peoplelog[1]=='leave'and peoplelog[0] in people:
+        people.remove(peoplelog[0])
+for i in range(len(people)):
+    print(people[i])
